35532cf0912386ccbb6194c3268518979b152432
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-06 14:12:57 +0100

    Fix hash(key) method
    Made % this.capacity instead of % key.length

--- src/hashmaps.js
+++ src/hashmaps.js
@@ -12,8 +12,10 @@ class HashMap {
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % key.length;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
     return hashCode;
   }
+
+  set(key, value) {}
 }
